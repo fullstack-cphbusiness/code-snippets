4707986f7f79b690588c898924d6ab6fc840b86c
Lars-m <lam@cphbusiness.dk>
2021-03-18 13:33:55 +0100

    Update apiError.ts

--- apiError.ts
+++ apiError.ts
@@ -5,19 +5,19 @@
 
 // ADD THIS FILE TO A FOLDER errors 
 
+//INFO: This is rewritten, compared to what we did in the class
 class ApiError extends Error {
-    constructor(msg:string,public errorCode ?:number) {
-      super(msg)
-  
-      // Maintains proper stack trace for where our error was thrown (only available on V8)
-      if (Error.captureStackTrace) {
-        Error.captureStackTrace(this, ApiError)
-      }
-  
-      this.name = 'ApiError'
-      this.errorCode = errorCode || 500;
-      
+  errorCode: number
+  constructor(msg: string, public eCode?: number) {
+    super(msg)
+
+    // Maintains proper stack trace for where our error was thrown (only available on V8)
+    if (Error.captureStackTrace) {
+      Error.captureStackTrace(this, ApiError)
     }
+    this.name = 'ApiError'
+    this.errorCode = eCode === undefined ? 500 : eCode
+  }
 }
 
-export {ApiError}
+export { ApiError }
